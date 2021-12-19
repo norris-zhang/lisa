5256d58d8c8b936e4982d24052528d6ff00ae27b
Norris Zhang <nzhang@bravurasolutions.com>
2021-12-19 16:53:03 +1300

    init

---
+++ src/main/java/com/norriszhang/lisa/LisaApplication.java
@@ -0,0 +1,13 @@
+package com.norriszhang.lisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/norriszhang/lisa/LisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norriszhang.lisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
